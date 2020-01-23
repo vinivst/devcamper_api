b1ca2c9eaa94a832fdf945868c79231c75701ba1
Vinicius Santiago Tenente <vinicius.santiago@gmail.com>
2020-01-22 21:24:19 -0300

    Back to early port

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.delete('/api/v1/bootcamps/:id', (req, res) => {
     .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
 });
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV}mode on port ${PORT}!`);
