ca63568728f3d27b392eac8f561bd9653b8b09e1
Vinicius Santiago Tenente <vinicius.santiago@gmail.com>
2020-01-25 19:32:24 -0300

    Connect to Database with Mongoose

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    })
+    .then(conn => {
+      console.log(
+        `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+      );
+    })
+    .catch(err => {
+      throw err;
+    });
+};
+
+module.exports = connectDB;
