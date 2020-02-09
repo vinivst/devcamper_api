ed711241f72fc35347db2bae3d8a0ac5ffd7eb80
Vinicius Santiago Tenente <vinicius.santiago@gmail.com>
2020-02-09 15:46:09 -0300

    Bootcamp & User Relantionship

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: './config/config.env' });
 //Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 //Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -27,45 +28,34 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
-//Import into DB
-const importData = () => {
-  Bootcamp.create(bootcamps)
-    .then(data => {
-      console.log('Bootcamps Imported...'.green.inverse);
-      //process.exit();
-    })
-    .catch(err => {
-      console.log(err);
-    });
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
 
-  Course.create(courses)
-    .then(data => {
-      console.log('Courses Imported...'.green.inverse);
-      process.exit();
-    })
-    .catch(err => {
-      console.log(err);
-    });
+//Import into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    await Course.create(courses);
+    await User.create(users);
+    console.log('Data Imported...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 //Delete data
-const deleteData = () => {
-  Bootcamp.deleteMany()
-    .then(data => {
-      console.log('Bootcamps Destroyed...'.red.inverse);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-
-  Course.deleteMany()
-    .then(data => {
-      console.log('Courses Destroyed...'.red.inverse);
-      process.exit();
-    })
-    .catch(err => {
-      console.log(err);
-    });
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    await Course.deleteMany();
+    await User.deleteMany();
+    console.log('Data Destroyed...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 if (process.argv[2] == '-i') {
