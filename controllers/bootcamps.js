5142ab942932009a0fc074c2e8bc5d7d7f403155
Vinicius Santiago Tenente <vinicius.santiago@gmail.com>
2020-02-09 16:00:55 -0300

    Bootcamp Ownership

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -60,16 +60,29 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @route       PUT /api/v1/bootcamps/:id
 // @access      Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true
-  });
+  let bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  //Make sure user is bootcamp owner
+  if (bootcamp.user.toString() != req.user.id && req.user.role != 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorize to update this bootcamp`,
+        401
+      )
+    );
+  }
+
+  bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
   res.status(200).json({
     success: true,
     data: bootcamp
@@ -85,6 +98,16 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  //Make sure user is bootcamp owner
+  if (bootcamp.user.toString() != req.user.id && req.user.role != 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorize to delete this bootcamp`,
+        401
+      )
     );
   }
 
@@ -132,6 +155,16 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  //Make sure user is bootcamp owner
+  if (bootcamp.user.toString() != req.user.id && req.user.role != 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorize to upload a photo for this bootcamp`,
+        401
+      )
     );
   }
 
