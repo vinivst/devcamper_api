ec9935df0c53ceb6306ffc854bb30c342eb0874e
Vinicius Santiago Tenente <vinicius.santiago@gmail.com>
2020-02-09 17:27:31 -0300

    Forgot Password - Generate Token

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { register, login, getMe } = require('../controllers/auth');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword
+} = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -9,5 +14,6 @@ const { protect } = require('../middleware/auth');
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/me').get(protect, getMe);
+router.route('/forgotpassword').post(forgotPassword);
 
 module.exports = router;
