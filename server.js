172a494c5df11edf4315faeff91661d39f8cc130
Vinicius Santiago Tenente <vinicius.santiago@gmail.com>
2020-02-10 18:53:30 -0300

    XSS Protection & Security Headers

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const colors = require('colors');
 const fileupload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
 const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
@@ -27,10 +29,10 @@ const app = express();
 // Body parser
 app.use(express.json());
 
-//Cookier Parser
+// Cookier Parser
 app.use(cookieParser());
 
-//Dev logging middleware
+// Dev logging middleware
 if (process.env.NODE_ENV == 'development') {
   app.use(morgan('dev'));
 }
@@ -38,8 +40,14 @@ if (process.env.NODE_ENV == 'development') {
 // File uploading
 app.use(fileupload());
 
-//Sanitize data
+// Sanitize data
 app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// Prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -52,7 +60,7 @@ app.use('/api/v1/auth', auth);
 app.use('/api/v1/users', users);
 app.use('/api/v1/reviews', reviews);
 
-//Handle erros
+// Handle erros
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
@@ -63,9 +71,9 @@ const server = app.listen(PORT, () => {
   );
 });
 
-//Handle unhandle promise rejections
+// Handle unhandle promise rejections
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`.red);
-  //Close server and exit process
+  // Close server and exit process
   server.close(() => process.exit(1));
 });
