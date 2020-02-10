490cf5f5ef83304d45a7c00379e081dde8a064af
Vinicius Santiago Tenente <vinicius.santiago@gmail.com>
2020-02-09 21:05:18 -0300

    Admin Users CRUD

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -10,13 +10,13 @@ const {
 } = require('../controllers/bootcamps');
 
 const Bootcamp = require('../models/Bootcamp');
-const advancedResults = require('../middleware/advancedResults');
 
 //Include other resources routers
 const courseRouter = require('./courses');
 
 const router = express.Router();
 
+const advancedResults = require('../middleware/advancedResults');
 //Import protect middleware from auth.js
 const { protect, authorize } = require('../middleware/auth');
 
